44e965e7b0aefa977e4f1685ce6a385e5ba730b4
Jay shah <shahjay444@gmail.com>
2019-06-18 12:08:01 +0530

    Added all parameters

--- template2.py
+++ template2.py
@@ -11,15 +11,34 @@ import db
 class Screen(FloatLayout):
     '''It is base layout that holds all our widgets'''
     cod_val = ObjectProperty(None)
+    bod_val = ObjectProperty(None)
+    tss_val = ObjectProperty(None)
+    ph_val = ObjectProperty(None)
+    flow_val = ObjectProperty(None)
     def temp_render(self, *arg):
         db_value = db.get_parameter_values()
         for x in db_value:
             # for key,value in x.items():
-            print(x)
             if x["parameter_id"] == "parameter_83":
-                temp = str(x["value"])
-                print("TEmp is ", temp)
-        self.cod_val.text = temp
+                cod = str(x["value"])
+            if x["parameter_id"] == "parameter_84":
+                bod = str(x["value"])
+            if x["parameter_id"] == "parameter_85":
+                tss = str(x["value"])
+            if x["parameter_id"] == "parameter_80":
+                ph = str(x["value"])
+            if x["parameter_id"] == "parameter_81":
+                flow = str(x["value"])
+        if cod:
+            self.cod_val.text = cod+" [size=15]mg/l[/size]"
+        if bod:
+            self.bod_val.text = bod+" [size=15]mg/l[/size]"
+        if tss:
+            self.tss_val.text = tss+" [size=15]mg/l[/size]"
+        if ph:
+            self.ph_val.text = ph
+        if flow:
+            self.flow_val.text = flow+" [size=15]m3/h[/size]"
     def show_datetime(self, *arg):
         self.timestamp.text = time.asctime()
 
