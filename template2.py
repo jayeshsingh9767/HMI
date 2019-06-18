71753b20c1e4900d14221f39f760df9f4a9d248e
Jay shah <shahjay444@gmail.com>
2019-06-18 11:51:10 +0530

    Added temp

--- template2.py
+++ template2.py
@@ -5,18 +5,29 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 import time
 from kivy.clock import Clock
+import db
 
-
+db_value = db.get_parameter_values()
+for x in db_value:
+    # for key,value in x.items():
+    print(x)
+    if x["parameter_id"] == "parameter_83":
+        temp = str(x["values"])
+        print("TEmp is ", temp)
 class Screen(FloatLayout):
     '''It is base layout that holds all our widgets'''
-    # timestamp = ObjectProperty(None)
+    cod_val = ObjectProperty(None)
+    def temp_render(self):
+        self.cod_val.text = temp
     def show_datetime(self, *arg):
         self.timestamp.text = time.asctime()
 
 
 class Template2App(App):
     def build(self):
-        return Screen()
+        obj = Screen()
+        Clock.schedule_interval(obj.temp_render, 1)
+        return obj
 
 
 if __name__ == "__main__":
